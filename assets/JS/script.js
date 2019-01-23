1ca74619f4ecbf482427db7357cb8ac00195308b
Gorma <sebayrton@hotmail.com>
2019-01-23 09:57:57 +0100

    JS partie 2 exercice 5

---
+++ assets/JS/script.js
@@ -0,0 +1,32 @@
+function changeimage(n) {
+// document.getElementById('image1').src='assets/img/image1_2.jpg' ;
+// document.getElementById('image2').src='assets/img/image2_2.jpg' ;
+// document.getElementById('image3').src='assets/img/image3_2.jpg' ;
+// document.getElementById('image4').src='assets/img/image4_2.jpg' ;
+// document.getElementById('image5').src='assets/img/image5_2.jpg' ;
+
+switch (n) {
+
+case 1:
+document.getElementById('image1').src='assets/img/image1_2.jpg';
+break ;
+
+case 2:
+document.getElementById('image2').src='assets/img/image2_2.jpg';
+break ;
+
+case 3:
+document.getElementById('image3').src='assets/img/image3_2.jpg';
+break ;
+
+case 4:
+document.getElementById('image4').src='assets/img/image4_2.jpg';
+break ;
+
+case 5:
+document.getElementById('image5').src='assets/img/image5_2.jpg';
+break ;
+
+}
+
+}
